6571db86beda72b61c2aed29788d079c04a0b12f
Keeneng Vang <keenenglvang@gmail.com>
2022-04-04 01:31:24 -0500

    Added gradient background

--- src/views/SearchWallet.tsx
+++ src/views/SearchWallet.tsx
@@ -1,15 +1,20 @@
 import { Button, SearchInput, TextInputField } from 'evergreen-ui';
 import { useState } from 'react';
 import './SearchWallet.scss';
+import logo from '../assets/logo.svg';
 
 function SearchWallet() {
   const [value, setValue] = useState('');
 
   return (
     <div className="vsto-search-wallet">
+      <header className="vsto-search-wallet-header">
+        <img className="vsto-search-wallet-header-logo" src={logo} alt="Logo" />
+      </header>
       <section className="vsto-search-wallet-input-container">
         <SearchInput
           className="search-input-box"
+          placeholder="Enter eth wallet address..."
           onChange={(e: any) => setValue(e.target.value)}
           value={value}
         />
@@ -18,7 +23,7 @@ function SearchWallet() {
           appearance="primary"
           size="large"
         >
-          Search Wallet
+          Submit
         </Button>
       </section>
     </div>
