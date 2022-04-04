cb37ff831e239667ea1bcc7007d613037c3f3705
unknown <keenenglvang@gmail.com>
2022-04-03 21:32:39 -0500

    Added evergreen-ui

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
   // };
 
   return (
-    <div>
+    <div className="vsto-container">
       {/* <h1>Hello World!</h1>
       <button onClick={login}>Moralis Metamask Login</button>
       <button onClick={logOut} disabled={isAuthenticating}>
--- src/views/SearchWallet.tsx
+++ src/views/SearchWallet.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { Button, SearchInput, TextInputField } from 'evergreen-ui';
+import { useState } from 'react';
+import './SearchWallet.scss';
 
 function SearchWallet() {
-  return <div>Search Wallet</div>;
+  const [value, setValue] = useState('');
+
+  return (
+    <div className="vsto-search-wallet">
+      <section className="vsto-search-wallet-input-container">
+        <SearchInput
+          className="search-input-box"
+          onChange={(e: any) => setValue(e.target.value)}
+          value={value}
+        />
+        <Button
+          className="search-input-button"
+          appearance="primary"
+          size="large"
+        >
+          Search Wallet
+        </Button>
+      </section>
+    </div>
+  );
 }
 
 export default SearchWallet;
